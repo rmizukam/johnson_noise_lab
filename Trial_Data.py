c380521152afc80387a35a6df2628602ad649dd3
Ryan Mizukami <rmizukami741@gmail.com>
2021-09-23 23:27:12 -0700

    commented out code

--- Trial_Data.py
+++ Trial_Data.py
@@ -1,6 +1,6 @@
 import numpy as np
 # import matplotlib.pyplot as plt
-# import math
+import math
 # from scipy.optimize import curve_fit
 # from scipy import integrate
 
@@ -9,12 +9,12 @@ class Trial_Data:
         self.data = data
         self.resistance = resistance
         self.gain = gain
-        self.correction_factor
-        self.corrected_data = self.correcting_data(self.correction_factor)   
+        # self.correction_factor
+        # self.corrected_data = self.correcting_data(self.correction_factor)   
 
     def error(self):
         return (np.amax(self.data) - np.amin(self.data))/(2*math.sqrt(len(self.data)))
     
-    def correcting_data(self,correction_factor):
-        self.correcting_data = np.subtract(self.data,correction_factor)
+    # def correcting_data(self,correction_factor):   # need to extrapolate a plot first
+        # self.correcting_data = np.subtract(self.data,correction_factor)
     
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # main
 
-from Trial_Data import Trial_data
+from Trial_Data import Trial_Data
 
-tri1 = Trial_data([0.977, 0.978, 0.976, 0.976, 0.977],1,2000)
+tri1 = Trial_Data([0.977, 0.978, 0.976, 0.976, 0.977],1,2000)
